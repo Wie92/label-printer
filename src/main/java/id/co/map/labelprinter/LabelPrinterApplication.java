00597bc71f572758a4d9357fd029631c4a81831d
Hendry Susanto <35400792+HotWie@users.noreply.github.com>
2019-10-04 08:39:32 +0700

    Initial Commit

---
+++ src/main/java/id/co/map/labelprinter/LabelPrinterApplication.java
@@ -0,0 +1,13 @@
+package id.co.map.labelprinter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabelPrinterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabelPrinterApplication.class, args);
+	}
+
+}
